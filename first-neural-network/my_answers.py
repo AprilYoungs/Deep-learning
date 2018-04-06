5ce744d8b01d4a56f6f19a82ad40722014467876
AprilYoungs <yangchangjun010@hotmail.com>
2018-04-06 23:34:21 +0800

    resubmit project

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -140,7 +140,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 1000
-learning_rate = 1
-hidden_nodes = 5
+iterations = 8000
+learning_rate = 1.3
+hidden_nodes = 9
 output_nodes = 1
